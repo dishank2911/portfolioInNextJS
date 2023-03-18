9b6495b5b7a6530a4a74df117732b7b265cf72f4
dishank2911 <dishank2911@gmail.com>
2023-03-18 14:34:26 +0530

    ..

--- app/head.jsx
+++ app/head.jsx
@@ -1,14 +1,13 @@
 export default function Head() {
-    return(
+    return (
         <>
-        <title>Portfolio</title>
-        <meta httpEquiv="X-UA-Compatible" content="IE=7" />
-        <meta name="description" content="Dishank Patel Personal Portfolio" />
-        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-        <meta name="keywords" content="portfolio,dishank,dishankpatel,personal portfolio" />
-        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+            <title>Portfolio</title>
+            <meta httpEquiv="X-UA-Compatible" content="IE=7" />
+            <meta name="description" content="Dishank Patel Personal Portfolio" />
+            <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+            <meta name="keywords" content="portfolio,dishank,dishankpatel,personal portfolio" />
+            <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         </>
     )
 }
